232cafd3eacdfde2a8ae2d73849963e11dbfca05
Helmianto <helmianto15@gmail.com>
2021-07-06 17:26:21 +0700

    initial commit

---
+++ tugas10.php
@@ -0,0 +1,23 @@
+<?php
+$arr = array(1,3,8,2,5,7,4,0);
+
+print("Sebelum Di Sorting :<br>");
+print_r($arr);
+
+$arr = bubble_sort($arr);
+print("<br> Setelah Disorting (Buble Sort) :<br>");
+print_r($arr);
+
+function bubble_sort($arr) {
+    for ($i=0; $i < count($arr); $i++){
+        for($j=0; $j < count($arr) - 1; $j++){
+            if($arr[$j] > $arr[$j+1]){
+                $temp = $arr[$j];
+                $arr[$j] = $arr[$j+1];
+                $arr[$j+1] = $temp;
+            }
+        }
+    }
+    return $arr;
+}
+?>
